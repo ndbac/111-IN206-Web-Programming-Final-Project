ace9e67a4fb39f4890e764acdd2784799d8200fd
ndbac <ryannguyen0303@gmail.com>
2023-01-06 14:42:37 +0800

    chore: add progress bar in acticle details page

--- pages/articles/[articleId].tsx
+++ pages/articles/[articleId].tsx
@@ -2,11 +2,18 @@ import type { NextPage } from 'next';
 import { NavigationBarDark } from '../../Components/Navigation/NavigationDark';
 import { Footer } from '../../Components/Footer/Footer';
 import { Articles } from '../../Components/Articles';
+import { motion, useScroll } from 'framer-motion';
 
 const ArticlesPage: NextPage = () => {
+  const { scrollYProgress } = useScroll();
+
   return (
     <>
       <NavigationBarDark />
+      <motion.div
+        className="fixed top-0 left-0 right-0 h-[8px] bg-blue-600 origin-left opacity-100 z-50"
+        style={{ scaleX: scrollYProgress }}
+      />
       <Articles />
       <Footer />
     </>
